1d462ec8cbb16778651da4ddf96b6fd4d8a8bd43
Nathan <newmannh@gmail.com>
2014-10-15 14:39:59 -0400

    added second form generation outline

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 	"code.google.com/p/gofpdf"
 )
 
-var bob = "bob"
+//TODO:
+//week -> inspection
+//generated numbers
+//change one of the items (see basecamp)
 
 func main() {
 	fmt.Printf("Hello, world.\n")
 	parseJson("example.json")
-	generatePdf()
+	generatePdf2()
 }
 
 func parseJson(filename string) {
@@ -33,7 +36,7 @@ func parseJson(filename string) {
 
 }
 
-func generatePdf() {
+func generatePdf1() {
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 	pdf.SetFont("Arial", "", 15)
@@ -55,9 +58,9 @@ func generatePdf() {
 	pdf.Text(initialColYs[1], top+10, "QUALITY")
 	_, fontSize := pdf.GetFontSize()
 	pdf.SetFontSize(11)
-	pdf.Text(initialColYs[1], top+10+fontSize, "Weekly Report")
+	pdf.Text(initialColYs[1], top+10+fontSize, "Inspection Report")
 	pdf.SetXY(initialColYs[2], top)
-	pdf.MultiCell(initialColWidths[2], 2, "\nContract #\n\n\n\n\nCustomer\n\n\n\n\n", "1", "T", false)
+	pdf.MultiCell(initialColWidths[2], 2, "\nFrac Co.\n\n\n\n\nOperator\n\n\n\n\n", "1", "T", false)
 	pdf.SetXY(initialColYs[3], top)
 	pdf.CellFormat(initialColWidths[3], 7, "", "1", 2, "", false, 0, "")
 	pdf.CellFormat(initialColWidths[3], 15, "", "1", 1, "", false, 0, "")
@@ -75,7 +78,7 @@ func generatePdf() {
 	pdf.Ln(lineHeight + 2)
 
 	pdf.SetFontSize(11)
-	pdf.CellFormat(0.5*width, lineHeight, "Week #:  ", "1", 0, "R", false, 0, "")
+	pdf.CellFormat(0.5*width, lineHeight, "Inspection #:  ", "1", 0, "R", false, 0, "")
 	numDivs := 12
 
 	printBlanks := func(numDivs int, wdth float64) {
@@ -97,7 +100,7 @@ func generatePdf() {
 	labels := []string{
 		"All functions operate properly",
 		"Structure and components secure and undamaged",
-		"Safety devices intact and functional",
+		"Shower and eyewash intact and functional",
 		"Operational and safety decals present and legible",
 		"Batteries Charged",
 		"Electrical cord/plug",
@@ -130,33 +133,168 @@ func generatePdf() {
 	pdf.Ln(lineHeight + 4)
 
 	pdf.SetFontSize(11)
-	pdf.CellFormat(0.9*width, lineHeight-2, "Weekly Notes:", "", 0, "", false, 0, "")
+	pdf.CellFormat(0.9*width, lineHeight-2, "Inspection Notes:", "", 0, "", false, 0, "")
 	pdf.CellFormat(0.1*width, lineHeight-2, "Initial", "1", 1, "C", false, 0, "")
 
 	for i := 1; i <= 12; i++ {
-		pdf.CellFormat(0.1*width, lineHeight, fmt.Sprintf("Week %d", i), "1", 0, "", false, 0, "")
-		pdf.CellFormat(0.8*width, lineHeight, "", "1", 0, "", false, 0, "")
+		pdf.CellFormat(0.2*width, lineHeight, "Inspection #: ", "1", 0, "", false, 0, "")
+		pdf.CellFormat(0.7*width, lineHeight, "", "1", 0, "", false, 0, "")
 		pdf.CellFormat(0.1*width, lineHeight, "", "1", 1, "", false, 0, "")
 	}
 
-	// pdf.MultiCell(initialColWidths[3], 2, "\nContract #\n\n\n\n\nCustomer\n\n\n\n\n", "1", "T", false)
-	// pdf.Cell(initialColWidths[1],10,"Quality")
-	// pdf.Cell(initialColWidths[1],10,"Weekly Report")
-	// pdf.Text(50, 20, "logo.png")
-	// pdf.Image(imageFile("logo.gif"), 10, 40, 30, 0, false, "", 0, "")
-	// pdf.Text(50, 50, "logo.gif")
-	// pdf.Image(imageFile("logo-gray.png"), 10, 70, 30, 0, false, "", 0, "")
-	// pdf.Text(50, 80, "logo-gray.png")
-	// pdf.Image(imageFile("logo-rgb.png"), 10, 100, 30, 0, false, "", 0, "")
-	// pdf.Text(50, 110, "logo-rgb.png")
-	// pdf.Image(imageFile("logo.jpg"), 10, 130, 30, 0, false, "", 0, "")
-	// pdf.Text(50, 140, "logo.jpg")
-	pdf.OutputFileAndClose("example.pdf")
+	pdf.OutputFileAndClose("example1.pdf")
 }
 
 func generatePdf2() {
-	pdf := gofpdf.New("P", "mm", "A4", "../font")
+	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(40, 10, "Hello")
+	pdf.SetFont("Arial", "", 8)
+	pdf.SetTopMargin(10)
+	pdf.SetLeftMargin(10)
+	pdf.SetRightMargin(10)
+	pdf.SetAutoPageBreak(true, 10)
+
+	pageW, _ := pdf.GetPageSize()
+	left, top, right, _ := pdf.GetMargins()
+	width := pageW - left - right
+
+	initialColWidths := []float64{0.5 * width, 0.25 * width, 0.25 * width}
+	initialColXs := []float64{
+		left,
+		left + initialColWidths[0],
+		left + initialColWidths[0] + initialColWidths[1]}
+
+	_, fontSize := pdf.GetFontSize()
+	pdf.SetXY(initialColXs[2], top)
+	lineHeight := 5 * fontSize
+	pdf.CellFormat(initialColWidths[2], lineHeight, "Pad Name", "1", 2, "LM", false, 0, "")
+	pdf.CellFormat(initialColWidths[2], lineHeight, "Operator", "1", 1, "LM", false, 0, "")
+
+	pdf.Image("ComplyLogo.png", left, top, 0.4*width, 0, false, "", 0, "")
+	pdf.SetXY(initialColXs[1], top)
+	pdf.SetFontSize(14)
+	pdf.CellFormat(initialColWidths[2], lineHeight, "QUALITY", "", 2, "CB", false, 0, "")
+	pdf.SetFontSize(10)
+	pdf.CellFormat(initialColWidths[2], lineHeight, "CONDITION REPORT", "", 1, "L", false, 0, "")
+
+	pdf.SetFontSize(8)
+	lineHeight = fontSize * 3
+
+	pdf.CellFormat(0.25*width, lineHeight, "Make", "1", 0, "L", false, 0, "")
+	pdf.CellFormat(0.25*width, lineHeight, "Model", "1", 0, "L", false, 0, "")
+	pdf.CellFormat(0.25*width, lineHeight, "Serial #", "1", 0, "L", false, 0, "")
+	pdf.CellFormat(0.25*width, lineHeight, "Equip #", "1", 1, "L", false, 0, "")
+
+	lineHeight = fontSize * 2
+	pdf.CellFormat(width, lineHeight, "MARK CLEARLY ALL DAMAGE BY SYMBOL        \"C\"=CUT    \"H\"=HOLE    \"D\"=DENT    \"P\"=PAINT DAMAGE", "1", 1, "C", false, 0, "")
+
+	colWidth := 0.49 * width
+	colXs := []float64{left, left + width - colWidth}
+
+	lineHeight = fontSize * 2
+	pdf.SetFontSize(7)
+	pdf.CellFormat(colWidth, lineHeight, "RENTAL OUT - Diagram & Identify any existing damages or strike-out box", "1", 0, "C", false, 0, "")
+	pdf.SetX(colXs[1])
+	pdf.CellFormat(colWidth, lineHeight, "RENTAL IN - Diagram & Identify any damages upon return", "1", 1, "C", false, 0, "")
+
+	pdf.CellFormat(colWidth, lineHeight*8, "", "1", 0, "C", false, 0, "")
+	pdf.SetX(colXs[1])
+	pdf.CellFormat(colWidth, lineHeight*8, "", "1", 1, "C", false, 0, "")
+
+	// lineHeight = lineHeight * 1.2
+
+	checkboxWidth := 0.04 * width
+	pdf.CellFormat(colWidth-2*checkboxWidth, lineHeight, "Inspection: Check if acceptable in accordance with Company spec", "1", 0, "", false, 0, "")
+	pdf.CellFormat(checkboxWidth, lineHeight, "OUT", "1", 0, "C", false, 0, "")
+	pdf.CellFormat(checkboxWidth, lineHeight, "IN", "1", 0, "C", false, 0, "")
+	pdf.SetX(colXs[1])
+	pdf.CellFormat(colWidth, lineHeight, "Comments - IN (if damaged, provide details of occurence)", "1", 1, "", false, 0, "")
+
+	items := []string{
+		"Structure and components secure and undamaged",
+		"All functions operate properly",
+		"Safety devices intact and functional",
+		"Operational and safety decals present and legible",
+		"Batteries charged",
+		"Electrical cord/plug",
+		"Containment tank      Qty pumps:",
+		"Appearance Acceptable - note and mark exceptions in diagram",
+		"Fire extinguisher        Qty:",
+		"First Aid",
+		"AED",
+		"",
+		"",
+	}
+
+	for _, item := range items {
+		pdf.CellFormat(colWidth-2*checkboxWidth, lineHeight, item, "1", 0, "", false, 0, "")
+		pdf.CellFormat(checkboxWidth, lineHeight, "", "1", 0, "C", false, 0, "")
+		pdf.CellFormat(checkboxWidth, lineHeight, "", "1", 0, "C", false, 0, "")
+		pdf.SetX(colXs[1])
+		pdf.CellFormat(colWidth, lineHeight, "", "1", 1, "", false, 0, "")
+	}
+
+	weirdLine := func(col1Text, col2Text string, lnHeight float64) {
+		pdf.CellFormat(colWidth, lnHeight, col1Text, "1", 0, "", false, 0, "")
+		pdf.SetX(colXs[1])
+		pdf.CellFormat(colWidth, lnHeight, col2Text, "1", 1, "", false, 0, "")
+	}
+
+	weirdLine("Towable equipment-Record license plate of towing vehicle:", "", lineHeight)
+	weirdLine("Comments - OUT ( list additional equipment )", "", lineHeight)
+	for i := 0; i < 8; i++ {
+		weirdLine("", "", lineHeight)
+	}
+
+	tab := func(wdth float64) string {
+		spaceW := pdf.GetStringWidth(" ")
+		numSpaces := wdth / spaceW
+		tabStr := ""
+		for i := 0.0; i < numSpaces; i += 1 {
+			tabStr += " "
+		}
+		return tabStr
+	}
+
+	weirdLine(
+		"Inspected and condtion OUT confirmed as specified above:",
+		"Inspected and condition IN confirmed as specified above:", lineHeight)
+
+	twoTabbedWeirdLine := func(col1First, col1Sec, col2First, col2Sec string, lnHeight float64) {
+		col1FirstW := pdf.GetStringWidth(col1First)
+		col2FirstW := pdf.GetStringWidth(col2First)
+
+		tabStart := 0.55 * colWidth
+		col1TabWidth := tabStart - col1FirstW
+		col2TabWidth := tabStart - col2FirstW
+
+		weirdLine(
+			col1First+tab(col1TabWidth)+col1Sec,
+			col2First+tab(col2TabWidth)+col2Sec,
+			lnHeight)
+	}
+
+	twoTabbedWeirdLine("Compliance Services by", "Print Name", "Compliance Services by", "Print Name", lineHeight)
+	twoTabbedWeirdLine("Date", "Date Delivered", "Date", "Date Received", lineHeight)
+
+	// _, multiCellLineHeight := pdf.GetFontSize()
+	// pdf.CellFormat(width, lineHeight,
+	// 	"CUSTOMER ACKNOWLEDGEMENT: THE SAFETY AND PERFORMANCE OF THIS EQUIPMENT"+
+	// 		" HAS BEEN VERIFIED. AS USER OF THIS EQUIPMENT, I UNDERSTAND THE CORRECT"+
+	// 		" OPERATION AND FUNCTION OF THE CONTROLS AND CONFIRM THAT I HAVE RECEIVED"+
+	// 		" ADEQUATE INSTRUCTION AND HAVE ADHERED TO THE SAFETY SHEET, THUS ENABLING MYSELF"+
+	// 		" AND/OR MY CREW TO USE THE EQUIPMENT IN A SAFE AND PROPER MANNER WITHOUT RISK OF INJURY.\n",
+	// 	"1", 1, "", false, 0, "")
+	pdf.MultiCell(width, 3,
+		"\nCUSTOMER ACKNOWLEDGEMENT: THE SAFETY AND PERFORMANCE OF THIS EQUIPMENT"+
+			" HAS BEEN VERIFIED. AS USER OF THIS EQUIPMENT, I UNDERSTAND THE CORRECT"+
+			" OPERATION AND FUNCTION OF THE CONTROLS AND CONFIRM THAT I HAVE RECEIVED"+
+			" ADEQUATE INSTRUCTION AND HAVE ADHERED TO THE SAFETY SHEET, THUS ENABLING MYSELF"+
+			" AND/OR MY CREW TO USE THE EQUIPMENT IN A SAFE AND PROPER MANNER WITHOUT RISK OF INJURY.\n\n",
+		"1", "", false)
+
+	weirdLine("Customer by", "Customer by", lineHeight)
+	twoTabbedWeirdLine("Print Name", "Date", "Print Name", "Date", lineHeight)
+
+	pdf.OutputFileAndClose("example2.pdf")
 }
